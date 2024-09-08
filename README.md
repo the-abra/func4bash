32729c2ba8a12b9cbca55ed35e29f2c4842fb868
Ege KILIÇ <83769871+the-abra@users.noreply.github.com>
2024-09-08 20:10:32 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,49 @@
+# Bash Utilities Library
+
+Welcome to a collection of libraries containing various functions written in Bash! This project aims to shorten and make more useful various special codes that are normally long and hard to remember.
+
+## Contents
+
+- [Features](#features)
+- [Installation](#installation)
+- [Libraries](#libraries)
+- [Contributing](#contributing-finding)
+- [License](#license)
+
+## Features
+
+- Color codes and style functions for terminal output
+- Functions to shorten default and frequently used file paths
+- Date and time formatting, calculation functions
+- Text editing, search and replace functions
+- File read, write, copy and delete operations
+- Network related operations and IP address query functions
+- Functions to query system status and information
+- Getting information from the user, menus and selection operations
+- Simple animations and effects in the terminal
+- Simple database operations (SQLite)
+- Making requests such as HTTP GET, POST
+- Reading and writing JSON data
+- Logging operations and log file management
+- Check and change file and directory permissions
+
+## Installation 🛠️
+
+    bash -c "$(curl -sSfL https://raw.githubusercontent.com/func4bash/bash-utilities-library/main/setup.sh)"
+
+## Libraries
+
+- **colors.lib**: Color codes and style functions for terminal output
+- **paths.lib**: Functions to shorten default and frequently used file paths
+- **datetime.lib**: Date and time formatting, calculation functions
+- **textprocessing.lib**: Text editing, search and replace functions
+- **fileoperations.lib**: File read, write, copy and delete operations
+- **network.lib**: Network related operations and IP address query functions
+- **sysinfo.lib**: Functions for querying system status and information
+- **userinput.lib**: Getting information from the user, menus and selection operations
+- **static_process_bar.lib**: Simple process bar and effects in terminal
+- **database.lib**: Simple database operations (SQLite)
+- **http.lib**: Making requests such as HTTP GET, POST
+- **json.lib**: Read and write JSON data
+- **logging.lib**: Logging operations and log file management
+- **permissions.lib**: Check and change file and directory permissions
---
+++ setup.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+if ! [ "$UID" -eq 0 ]; then
+    echo "You are not running as root. Please run this script with sudo or as root."
+    exit 1
+fi
+
+if ! ping -c 1 github.com > /dev/null; then
+    echo -e "You must have an internet connection!" && exit 1
+fi
+
+
+# Functions
+errorlog() {
+    echo -e "${RESEST}[${RED}${BLINK}ERROR${STOPBLINK}${RESEST}] $1" && sleep 0.2
+}
+warnlog() {
+    echo -e "${RESET}[${YELLOW}WARN${RESET}] $1" && sleep 0.2
+}
+successlog() {
+    echo -e "${RESET}[${GREEN}SUCCESS${RESET}] $1" && sleep 0.2
+}
+infolog() {
+    echo -e "${RESET}[${CYAN}INFO${RESET}] $1" && sleep 0.2
+}
+
+# Function to determine the package manager and install a package
+install_package() {
+    local package=$1
+    if command -v apt >/dev/null; then
+        apt install -yq "$package" &> /dev/null
+    elif command -v pacman >/dev/null; then
+        pacman -S --noconfirm "$package" &> /dev/null
+    else
+        errorlog "No suitable package manager found. Please install $package manually."
+        exit 1
+    fi
+}
+
+
+check_and_install() {
+    if ! command -v $1 >/dev/null; then
+        infolog "Installing $2"
+        install_package $2
+        if ! command -v $1 >/dev/null; then
+            errorlog "$2 installation failed, try manually."
+            exit 1
+        else
+            successlog "$2 installed."
+        fi
+    else
+        infolog "$2 found."
+    fi
+}
+
+# Update package lists
+apt update &> /dev/null
+pacman -Syu --noconfirm &> /dev/null
+
+! command -v curl >/dev/null && install_package install curl &> /dev/null
+
+curl https://raw.githubusercontent.com/func4bash/bash-utilities-library/main/colors.lib -s -o /tmp/colors.lib
+if [[ $(cat "/tmp/colors.lib" | head -n1) =~ "# Define color variables" ]]; then
+    source "/tmp/colors.lib"
+else
+    errorlog "Colors not imported, you have to check github.com connection."
+fi
+
+# Check and install Git
+check_and_install git git
+
+# Move /lib/func4bash to /lib/func4bash.old if it exists
+if [ -d /lib/func4bash ]; then
+    [[ -d /lib/func4bash.old ]] && rm -r /lib/func4bash.old
+    mv /lib/func4bash /lib/func4bash.old
+    infolog "Moving /lib/func4bash to /lib/func4bash.old"
+fi
+
+cd /lib/
+git clone https://github.com/func4bash/bash-utilities-library.git > /dev/null 2>&1
+mv bash-utilities-library func4bash
+# Check if the clone was successful
+if [ ! -d /lib/func4bash ]; then
+    errorlog "System cloning process went wrong. \ncode: git clone https://github.com/func4bash/bash-utilities-library.git"
+    exit 1
+fi
+
+[[ -f setup.sh ]] && rm setup.sh
+rm /tmp/colors.lib
+
+cd /lib/func4bash
+git rm -r --cached . &> /dev/null
+
+successlog "System files installed."
+exit 0 
